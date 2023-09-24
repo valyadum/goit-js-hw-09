ae8717f2d9b1715f6d9f1cae87f7757854c44cbb
valyadum <113098511+valyadum@users.noreply.github.com>
2023-09-24 06:26:56 +0000

    Deploying to gh-pages from @ valyadum/goit-js-hw-09@6d343e1fded40c952e35cc10355ac1030cccc4eb 🚀

---
+++ 03-promises.4f91d497.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e,t){Math.random(),function(e){e.preventDefault();var t=e.currentTarget,n=t.elements.amount.value,a=t.elements.delay.value,l=t.elements.step.value;console.log(n,a,l)}(number,firstDelay,stepDelay)}));
+//# sourceMappingURL=03-promises.4f91d497.js.map
